e535376284e9a859e69e216e6f77e2285f3ec855
nuevoalex <achap5dk@gmail.com>
2021-12-07 15:12:24 -0800

    format

--- tools/eco.js
+++ tools/eco.js
@@ -189,7 +189,9 @@ async function deployEco() {
       const faucet = new web3.eth.Contract(EcoFaucetABI.abi, faucetaddr);
 
       const mintAmount = web3.utils.toWei('500000', 'ether');
-      await faucet.methods.mint(options.account, mintAmount).send({ from: options.account, gas: 1000000 });
+      await faucet.methods.mint(options.account, mintAmount).send(
+        { from: options.account, gas: 1000000 },
+      );
     }
     console.log(`ERC20 at ${options.erc20}`);
   }
