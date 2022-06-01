cef070a0757854ff9132b5917d06339bbf6bc660
Albert Brown <albert@eco.com>
2022-06-01 10:20:47 -0700

    Eco 161 (#333)
    * fixed a lingering bad import
    
    * added some confirmations for transfer events in test
    
    * added an event of type BaseValueTransfer to mark gons transfers
    
    * tests for new event
    
    * prettier
    
    Co-authored-by: Albert Brown <albertbrown@Bobingy.local>

--- contracts/test/TokenTarget.sol
+++ contracts/test/TokenTarget.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../currency/ERC20.sol";
 
 /** @title Target for approveAndCall test */
 contract TokenTarget {
