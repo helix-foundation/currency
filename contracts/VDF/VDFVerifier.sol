f9fd87d2173f9f7db68389e30126b582f9a43177
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2022-07-26 13:00:35 -0400

    upping the prime iterations to 25 from 10 (#417)

--- contracts/VDF/VDFVerifier.sol
+++ contracts/VDF/VDFVerifier.sol
@@ -19,7 +19,8 @@ contract VDFVerifier is PolicedUtils, IsPrime {
 
     bytes public constant N =
         hex"c7970ceedcc3b0754490201a7aa613cd73911081c790f5f1a8726f463550bb5b7ff0db8e1ea1189ec72f93d1650011bd721aeeacc2acde32a04107f0648c2813a31f5b0b7765ff8b44b4b6ffc93384b646eb09c7cf5e8592d40ea33c80039f35b4f14a04b51f7bfd781be4d1673164ba8eb991c2c4d730bbbe35f592bdef524af7e8daefd26c66fc02c479af89d64d373f442709439de66ceb955f3ea37d5159f6135809f85334b5cb1813addc80cd05609f10ac6a95ad65872c909525bdad32bc729592642920f24c61dc5b3c3b7923e56b16a4d9d373d8721f24a3fc0f1b3131f55615172866bccc30f95054c824e733a5eb6817f7bc16399d48c6361cc7e5";
-    uint8 public constant ITERATIONS = 10;
+    //number of iterations to check a probable prime by
+    uint8 public constant PRIME_VALIDATE_ITERATIONS = 25;
     uint8 public constant MIN_BYTES = 64;
 
     /* The State is a data structure that tracks progress of a logical single verification session
@@ -79,7 +80,10 @@ contract VDFVerifier is PolicedUtils, IsPrime {
 
         require(_x > 1, "The commitment (x) must be > 1");
 
-        require(isProbablePrime(_x, ITERATIONS), "x must be probable prime");
+        require(
+            isProbablePrime(_x, PRIME_VALIDATE_ITERATIONS),
+            "x must be probable prime"
+        );
 
         BigNumber.Instance memory n = BigNumber.from(N);
         BigNumber.Instance memory x = BigNumber.from(_x);
