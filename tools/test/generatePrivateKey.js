f29dbd771d256850c1241d801f9fb46c341acaae
Albert Brown <albertbrown@Bobingy.local>
2022-08-31 14:05:27 -0700

    remove explicit bip39 dependency

--- tools/test/generatePrivateKey.js
+++ tools/test/generatePrivateKey.js
@@ -1,11 +1,8 @@
 /* eslint-disable no-console, no-underscore-dangle */
 
 // THIS IS FOR USE IN TESTNET TESTING
-const bip39 = require('bip39')
 const ethers = require('ethers')
 
-const mnemonic = bip39.generateMnemonic()
-
-const wallet = ethers.Wallet.fromMnemonic(mnemonic)
+const wallet = ethers.Wallet.createRandom()
 
 console.log(`private key: ${wallet._signingKey().privateKey}`)
