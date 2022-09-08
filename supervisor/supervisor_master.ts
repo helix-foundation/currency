a0687bf1fee0137341e95b8d7bd30607c0b23362
nknavkal <nknavkal@gmail.com>
2022-09-08 00:47:57 -0400

    trying to keep master supervisor updated

--- supervisor/supervisor_master.ts
+++ supervisor/supervisor_master.ts
@@ -21,12 +21,13 @@ let pk = process.env.PRIVATE_KEY || "";
 const ID_TIMED_POLICIES = ethers.utils.solidityKeccak256(['string'], ['TimedPolicies'])
 const ID_CURRENCY_GOVERNANCE = ethers.utils.solidityKeccak256(['string'], ['CurrencyGovernance'])
 
-let provider: ethers.providers.BaseProvider
-
 export class Supervisor {
     timeGovernor!: TimeGovernor
     currencyGovernor!: CurrencyGovernor
     inflationGovernor!: InflationGovernor
+    provider!: ethers.providers.BaseProvider
+    rootPolicy!: Policy
+    wallet!: ethers.Signer
 
 
 
@@ -35,24 +36,30 @@ export class Supervisor {
     }
 
 
-    async startSupervisor (filepath: string) {
-        let args = fs.readFileSync(filepath);
-        args = args.toString().split('\n');
-        let rpc: string = args[0];
-        let root: string = args[1];
-        provider = new ethers.providers.JsonRpcProvider(rpc);
-        let supervisorWallet: ethers.Wallet = new ethers.Wallet(pk, provider);
-        let rootPolicy: Policy = Policy__factory.connect(root, supervisorWallet);
-        this.startModules(supervisorWallet, rootPolicy);
+    async startSupervisor (filepath?: string, policy?: Policy, signer?: ethers.Signer) {
+        if (filepath) {
+            // prod
+            let args = fs.readFileSync(filepath);
+            args = args.toString().split('\n');
+            let rpc: string = args[0];
+            let root: string = args[1];
+            this.provider = new ethers.providers.JsonRpcProvider(rpc);
+            this.wallet = new ethers.Wallet(pk, this.provider);
+            this.rootPolicy = Policy__factory.connect(root, this.wallet);
+        } else {
+            // test
+            if(signer && policy) {
+                this.provider = hre.ethers.provider
+                this.wallet = signer
+                this.rootPolicy = policy
+            }
+        }
+    
+        this.startModules(this.provider, this.wallet, this.rootPolicy);
     
     }
     
-    async testStartSupervisor(rootPolicy: Policy, account: SignerWithAddress) {
-        provider = hre.ethers.provider
-        return this.startModules(account, rootPolicy);
-    }
-    
-    async startModules(wallet: ethers.Signer, rootPolicy: Policy) {
+    async startModules(provider: ethers.providers.BaseProvider, wallet: ethers.Signer, rootPolicy: Policy) {
         let timedPolicy: TimedPolicies = TimedPolicies__factory.connect(
             await rootPolicy.policyFor(ID_TIMED_POLICIES),
             wallet
@@ -71,37 +78,3 @@ export class Supervisor {
         this.inflationGovernor.setup()
     }
 }
-
-// export async function startSupervisor (filepath: string) {
-//     let args = fs.readFileSync(filepath);
-//     args = args.toString().split('\n');
-//     let rpc: string = args[0];
-//     let root: string = args[1];
-//     provider = new ethers.providers.JsonRpcProvider(rpc);
-//     let supervisorWallet: ethers.Wallet = new ethers.Wallet(pk, provider);
-//     let rootPolicy: Policy = Policy__factory.connect(root, supervisorWallet);
-//     startModules(supervisorWallet, rootPolicy);
-
-// }
-
-// export async function testStartSupervisor(rootPolicy: Policy, account: SignerWithAddress) {
-//     provider = hre.ethers.provider
-//     return  startModules(account, rootPolicy);
-// }
-
-// async function startModules(wallet: ethers.Signer, rootPolicy: Policy) {
-//     let timedPolicy: TimedPolicies = TimedPolicies__factory.connect(
-//         await rootPolicy.policyFor(ID_TIMED_POLICIES),
-//         wallet
-//     );
-//     let currencyGovernance: CurrencyGovernance = CurrencyGovernance__factory.connect(await rootPolicy.policyFor(ID_CURRENCY_GOVERNANCE), wallet)
-
-//     let timeGovernor: TimeGovernor = new TimeGovernor(provider, wallet, rootPolicy, timedPolicy)
-//     await timeGovernor.startTimer()
-
-//     let currencyGovernor: CurrencyGovernor = new CurrencyGovernor(provider, wallet, rootPolicy, timedPolicy, currencyGovernance)
-//     await currencyGovernor.setup()
-//     await currencyGovernor.startTimer()
-
-//     return ([timeGovernor, currencyGovernor])
-// }
