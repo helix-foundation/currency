4c0fb358b5f8638b306d0c28aeb00f49a1835aab
Nishaad Navkal <nknavkal@gmail.com>
2022-10-05 17:08:29 -0500

    Eco 174 (#543)
    * initial
    
    * done
    
    * fixing PR comments
    
    Co-authored-by: Albert Brown <albertbrown@Bobingy.local>

---
+++ contracts/governance/community/AccessRootPolicyFunds.propo.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.8.0;
+
+import "../../policy/Policy.sol";
+import "../../policy/Policed.sol";
+import "./Proposal.sol";
+import "../../currency/ECO.sol";
+import "../../currency/ECOx.sol";
+
+/** @title DeployRootPolicyFundw
+ * A proposal to send some root policy funds to another
+ * address (multisig, lockup, etc)
+ */
+contract AccessRootPolicyFunds is Policy, Proposal {
+    address public immutable recipient;
+
+    uint256 public immutable ecoAmount;
+
+    uint256 public immutable ecoXAmount;
+
+    constructor(
+        address _recipient,
+        uint256 _ecoAmount,
+        uint256 _ecoXAmount
+    ) {
+        recipient = _recipient;
+        ecoAmount = _ecoAmount;
+        ecoXAmount = _ecoXAmount;
+    }
+
+    function name() public pure override returns (string memory) {
+        return "Root Policy Funds Use Template";
+    }
+
+    function description() public pure override returns (string memory) {
+        return
+            "Sends ecoAmount and ecoXAmount of ECO and ECOx respectively to recipient";
+    }
+
+    function url() public pure override returns (string memory) {
+        return
+            "https://description.of.proposal make this link to a discussion what the funds are used for";
+    }
+
+    function enacted(address) public override {
+        bytes32 _ecoID = keccak256("ECO");
+        ECO eco = ECO(policyFor(_ecoID));
+
+        bytes32 _ecoXID = keccak256("ECOx");
+        ECOx ecoX = ECOx(policyFor(_ecoXID));
+
+        // if either ecoAmount or ecoXAmount are zero, parts related to that token should instead be removed
+        eco.transfer(recipient, ecoAmount);
+        ecoX.transfer(recipient, ecoXAmount);
+    }
+}
---
+++ test/governance/AccessRootPolicyFunds.js
@@ -0,0 +1,142 @@
+const { expect } = require('chai')
+const time = require('../utils/time.ts')
+const { ecoFixture, policyFor } = require('../utils/fixtures')
+const { deploy } = require('../utils/contracts')
+
+const stake = ethers.utils.parseEther('5000')
+
+describe('E2E Test to access funds in treasury [@group=9]', () => {
+  describe('sends eco and ecox from the root policy to the charlie address', async () => {
+    let policy
+    let eco
+    let ecox
+    let timedPolicies
+    let policyProposals
+    let policyVotes
+    let initInflation
+    let accessRootPolicyFunds
+
+    let alice
+    let bob
+    let charlie
+    let dave
+
+    it('Deploys the production system', async () => {
+      const accounts = await ethers.getSigners()
+      ;[alice, bob, charlie, dave] = accounts
+      ;({
+        policy,
+        eco,
+        ecox,
+        faucet: initInflation,
+        timedPolicies,
+      } = await ecoFixture())
+    })
+
+    it('Stakes accounts', async () => {
+      /* Until we have some idea how initial distribution is done, this *does* use
+       *a test-function
+       */
+      await initInflation.mint(await alice.getAddress(), stake)
+      await initInflation.mint(await bob.getAddress(), stake)
+      await initInflation.mint(await dave.getAddress(), 500)
+
+      await initInflation.mintx(await dave.getAddress(), 200)
+    })
+
+    it('transfers some funds to the treasury', async () => {
+      await eco.connect(dave).transfer(policy.address, 500)
+      await ecox.connect(dave).transfer(policy.address, 200)
+
+      expect(await eco.balanceOf(policy.address)).to.equal(500)
+      expect(await ecox.balanceOf(policy.address)).to.equal(200)
+    })
+
+    it('Waits a generation', async () => {
+      await time.increase(3600 * 24 * 14)
+      await timedPolicies.incrementGeneration()
+    })
+
+    it('Constructs the proposal', async () => {
+      accessRootPolicyFunds = await deploy(
+        'AccessRootPolicyFunds',
+        await charlie.getAddress(),
+        500,
+        200
+      )
+      const name = await accessRootPolicyFunds.name()
+      expect(name).to.equal('Root Policy Funds Use Template')
+      expect(await accessRootPolicyFunds.description()).to.equal(
+        'Sends ecoAmount and ecoXAmount of ECO and ECOx respectively to recipient'
+      )
+      expect(await accessRootPolicyFunds.url()).to.equal(
+        'https://description.of.proposal make this link to a discussion what the funds are used for'
+      )
+
+      expect(await accessRootPolicyFunds.recipient()).to.equal(
+        await charlie.getAddress()
+      )
+      expect(await accessRootPolicyFunds.ecoAmount()).to.equal(500)
+      expect(await accessRootPolicyFunds.ecoXAmount()).to.equal(200)
+    })
+
+    it('Find the policy proposals instance', async () => {
+      const proposalsHash = ethers.utils.solidityKeccak256(
+        ['string'],
+        ['PolicyProposals']
+      )
+      policyProposals = await ethers.getContractAt(
+        'PolicyProposals',
+        await policyFor(policy, proposalsHash)
+      )
+    })
+
+    it('Accepts new proposals', async () => {
+      await eco
+        .connect(alice)
+        .approve(policyProposals.address, await policyProposals.COST_REGISTER())
+      await policyProposals
+        .connect(alice)
+        .registerProposal(accessRootPolicyFunds.address)
+
+      await time.increase(3600 * 24 * 2)
+    })
+
+    it('Adds stake to the proposal to ensure it goes to a vote', async () => {
+      await policyProposals
+        .connect(alice)
+        .support(accessRootPolicyFunds.address)
+      await policyProposals.connect(bob).deployProposalVoting()
+    })
+
+    it('Find the policy votes instance', async () => {
+      const policyVotesIdentifierHash = ethers.utils.solidityKeccak256(
+        ['string'],
+        ['PolicyVotes']
+      )
+      policyVotes = await ethers.getContractAt(
+        'PolicyVotes',
+        await policyFor(policy, policyVotesIdentifierHash)
+      )
+    })
+
+    it('Allows all users to vote', async () => {
+      await policyVotes.connect(alice).vote(true)
+      await policyVotes.connect(bob).vote(true)
+    })
+
+    it('Waits until the end of the voting period', async () => {
+      await time.increase(3600 * 24 * 7)
+    })
+
+    it('Executes the outcome of the votes', async () => {
+      expect(await eco.balanceOf(charlie.address)).to.equal(0)
+      expect(await ecox.balanceOf(charlie.address)).to.equal(0)
+
+      await policyVotes.execute()
+
+      expect(await eco.balanceOf(charlie.address)).to.equal(500)
+      expect(await ecox.balanceOf(charlie.address)).to.equal(200)
+    })
+  })
+})
