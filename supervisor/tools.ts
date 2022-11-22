5cab80b4b06e9690bb64acbb4f372bf11e049684
nknavkal <nknavkal@gmail.com>
2022-11-22 15:19:42 -0600

    prettier

--- supervisor/tools.ts
+++ supervisor/tools.ts
@@ -1,19 +1,21 @@
 import { logError, SupervisorError } from './logError'
 import * as ethers from 'ethers'
 
-export const fetchLatestBlock = async (provider: ethers.providers.BaseProvider) => {
-    let errors: any = []
-    for (let i = 0; i < 3; i++) {
-      try {
-        return await provider.getBlock('latest')
-      } catch (e) {
-        errors.push(e)
-        continue
-      }
+export const fetchLatestBlock = async (
+  provider: ethers.providers.BaseProvider
+) => {
+  const errors: any = []
+  for (let i = 0; i < 3; i++) {
+    try {
+      return await provider.getBlock('latest')
+    } catch (e) {
+      errors.push(e)
+      continue
     }
-    logError({
-      type: SupervisorError.InfuraIssue,
-      error: errors
-    })
-    throw new Error('Infura Issue')
   }
+  logError({
+    type: SupervisorError.InfuraIssue,
+    error: errors,
+  })
+  throw new Error('Infura Issue')
+}
